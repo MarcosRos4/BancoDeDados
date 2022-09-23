1c3d44dcf5344638440d5c1e207650e8f75041a3
Marcos Vinicius Cavalcante Rosa <103190249+MarcosRos4@users.noreply.github.com>
2022-09-23 11:35:13 -0300

    Add files via upload

--- vendas.sql
+++ vendas.sql
@@ -3,12 +3,12 @@ create database loja;
 USE loja;
 
  create TABLE cliente(
-	codi smallint not null auto_increment,
+	cod_cli smallint not null auto_increment,
     nome varchar(50) not null,
     nasc date not null,
-    rg smallint(9) not null,
-    cpf smallint(11) not null,
-	primary key(codi)
+    rg int(9) not null,
+    cpf int(11) not null,
+	primary key(cod_cli)
 );
 
 create table vendedor
@@ -31,15 +31,42 @@ create table pedido (
 num_ped int auto_increment,
 prazo_entr smallint not null,
 cd_cli smallint not null,
-cd_vend int not null,
+cd_vend smallint not null,
 primary key (num_ped),
 foreign key (cd_cli) REFERENCES CLIENTE (cod_cli),
 foreign key (cd_vend) REFERENCES VENDEDOR (cod_vend));
 
 create table item_pedido (
-no_ped smallint not null ,
+no_ped int not null ,
 cd_prod smallint not null,
 qtd_ped float not null,
 foreign key (no_ped) REFERENCES PEDIDO (num_ped),
 foreign key (cd_prod) REFERENCES PRODUTO (cod_prod)
 ); 
+# funções de alterar a tabela DDL Data Definition Language
+alter table cliente rename column statuscli to molho;
+alter table pedido modify column prazo_entr char(8);
+alter table cliente change column molho statuscli bit;
+alter table cliente drop column statuscli;
+
+# funções de alterar dado DML Data Manipulation Language
+# insere um dado
+insert into item_pedido (no_ped, cd_prod, qtd_ped) values(5, 2, 7);
+
+# atualiza um dado
+update produto
+set val_unit = val_unit * 1.05;
+
+# deleta registros, deleta DADOS
+# não é possivel deletar um registro que sua chave primaria é usada em outra tabela
+delete from item_pedido
+where no_ped = 1;
+
+# mostra as tabelas inteiras
+select * from cliente;
+select * from vendedor;
+select * from produto;
+select * from item_pedido;
+select * from pedido;
+
+desc cliente;
